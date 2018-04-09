19b96140ceeac4862149b86d6db8441e6fdb9e0b
Ashish <ashish@airtory.com>
2018-04-09 14:39:37 +0530

    Recursive approach added

--- binarysearch.py
+++ binarysearch.py
@@ -1,3 +1,4 @@
+#Iterative Approach
 def binary_search(list,x):
     first= 0  
     last = len(list)
@@ -13,5 +14,22 @@ def binary_search(list,x):
                 first=mid + 1
     return False
 
-#print(binary_search([5,3,6,72,46]))
+#Recursive Approach
+def binary_search_recur(list,elem):
+    if(len(list)==0):
+        return False
+    else:
+        first=0
+        last= len(list)
+        mid= int((first+last)/2)
+        if(list[mid]==elem):
+            return True
+        else:
+            if(list[mid] > elem):
+                return binary_search(list[:mid-1],elem)
+            else:
+                return binary_search(list[mid+1:],elem)
+
+#print(binary_search_recur([5,3,6,72,46],56))
+# False
 #Time Complexity - Worst Case - O(nlogn), Best Case - O(1)
