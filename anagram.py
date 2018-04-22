4a24d0f2659dac0c24a6a7cbc29431754987f3af
Ashish <ashish@airtory.com>
2018-04-23 02:20:52 +0530

    Anagram check 2

--- anagram.py
+++ anagram.py
@@ -13,6 +13,8 @@ The Morse Code = Here comes dots
 def anagram1(w1,w2):
     w1= w1.replace(' ','').lower()
     w2= w2.replace(' ','').lower()
+    if(len(w1) != len(w2)):
+        return False
     if(sorted(w1)==sorted(w2)):
         return True
     else:
@@ -21,3 +23,30 @@ def anagram1(w1,w2):
 #time complexity - O(nlogn) //the time complexity of sorted()
 #print(anagram1("Here comes dots","The Morsse Code"))
 #output - True
+
+def anagram2(w1,w2):
+    w1= w1.replace(' ','').lower()
+    w2= w2.replace(' ','').lower()
+    # if(len(w1) != len(w2)):
+    #     return False
+    counter={}
+    for letter in w1:
+        if letter in counter:
+            counter[letter]+=1
+        else:
+            counter[letter]=1
+
+    for letter in w2:
+        if letter in counter:
+            counter[letter]-=1
+        else:
+            counter[letter]=1
+
+    for key in counter:
+        if counter[key]!=0:
+            return False 
+    return True   
+
+
+# print(anagram2("god","dog"))
+# Time complexity - O(n) where n is length of trimmed string
