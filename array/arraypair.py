37dbc36011928e0196dd5074916cdf21cb899abe
Ashish <ashish@airtory.com>
2018-04-23 10:45:38 +0530

    array pair problem  updated

--- array/arraypair.py
+++ array/arraypair.py
@@ -7,6 +7,8 @@ Output - (1,3)(2,2)
 
 #Approach 1
 def pair_sum(arr,k):
+    if(len(arr) < 2):
+        return "Array size is too small to find pairs"
     occured=set()
     for val1 in arr:
         for val2 in arr:
@@ -23,4 +25,24 @@ def pair_sum(arr,k):
 # Time complexity - O(n^2) as there are two loops(nested).
 
 
+#Approach 2 - Aim is to reduce time complexity
 
+def arraypair2(arr,k):
+    if(len(arr) < 2):
+        return "Array size is too small to find pairs"
+    seen=set()
+    output=set()
+
+    for val in arr:
+        target = k - val
+        if target in seen:
+            output.add((min(val,target),max(val,target)))
+        else:
+            seen.add(val)
+    
+    print("\n ".join(map(str,list(output))))
+
+
+arraypair2([1,2,3,2],4)
+
+# Time complexity - O(n) as there is only one loop.
